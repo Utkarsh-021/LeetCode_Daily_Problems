77514df28a6fecf2af906635b422c872e926fd08
Utkarsh-021 <42829401+Utkarsh-021@users.noreply.github.com>
2022-06-19 02:53:09 +0530

    Time: 2 ms (63.04%), Space: 41.7 MB (33.55%) - LeetHub

--- 258-add-digits/258-add-digits.java
+++ 258-add-digits/258-add-digits.java
@@ -1,5 +1,7 @@
 class Solution {
     public int addDigits(int num) {
+        /*
+        using loop
         int sum=0;
         while(num>0){
             sum+=num%10;
@@ -8,7 +10,10 @@ class Solution {
                 num=sum;
                 sum=0;
             }
-        }
+        } 
         return sum;
+        */
+        if(num==0) return 0;
+        else return num%9==0 ? 9:num%9;
     }
 }
